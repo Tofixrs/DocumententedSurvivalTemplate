5b3e990a836529616494ac644cc4c57b5fdd223d
tofixrs <tofix.rs@proton.me>
2024-02-02 16:59:07 +0100

    Documented BaseCharacter.lua

--- Scripts/game/characters/BaseCharacter.lua
+++ Scripts/game/characters/BaseCharacter.lua
@@ -1,264 +1,70 @@
-BaseCharacter = class( nil )
+---@class BaseCharacterCl
+---@field waterMovementSpeedFraction number Movement multiplier in water
+
+---@class BaseCharacterSv
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function BaseCharacter.server_onCreate( self )
-	self.character.publicData = {}
-	self.sv = {}
-
-
-
-
-
+---@class BaseCharacter : CharacterClass
+---@field cl BaseCharacterCl
+---@field sv BaseCharacterSv
+---@field graphicsLoaded boolean
+BaseCharacter = class(nil)
+function BaseCharacter:server_onCreate()
+    self.character.publicData = {}
+    self.sv = {}
 end
 
-function BaseCharacter.server_onFixedUpdate( self, timeStep )
-	-- Transfer server public data of water movement to client
-	self.cl.waterMovementSpeedFraction = 1.0
-	if self.character.publicData and self.character.publicData.waterMovementSpeedFraction then
-		self.cl.waterMovementSpeedFraction = self.character.publicData.waterMovementSpeedFraction
-	end
+---Inits the client data and sets player nametag
+function BaseCharacter:client_onCreate()
+    --init data
+    self.character.clientPublicData = {}
+    self.cl = {
+        waterMovementSpeedFraction = 1.0
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    --set player nametag
+    local player = self.character:getPlayer()
+    if sm.exists(player) then
+        if player ~= sm.localPlayer.getPlayer() then
+            local name = player:getName()
+            self.character:setNameTag(name)
+        else
+            -- if setNameTag is called here you will be able to see your own name, can be changed during runtime
+        end
+    end
 end
 
-
-
-
-
-
-
-
-
-
-
-
-
-function BaseCharacter.client_onCreate( self )
-	self.character.clientPublicData = {}
-	self.cl = {}
-	self.cl.waterMovementSpeedFraction = 1.0
-
-	local player = self.character:getPlayer()
-	if sm.exists( player ) then
-		if player ~= sm.localPlayer.getPlayer() then
-			local name = player:getName()
-			self.character:setNameTag( name )
-		else
-			-- if setNameTag is called here you will be able to see your own name, can be changed during runtime
-		end
-	end
-
-
-
-
+---Transfer server public data of water movement to client
+function BaseCharacter:server_onFixedUpdate()
+    self.cl.waterMovementSpeedFraction = 1.0
+    if self.character.publicData and self.character.publicData.waterMovementSpeedFraction then
+        self.cl.waterMovementSpeedFraction = self.character.publicData.waterMovementSpeedFraction
+    end
 end
 
-
-
-
-
-
-
-
-
-function BaseCharacter.client_onGraphicsLoaded( self )
-	self.graphicsLoaded = true
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function BaseCharacter:client_onGraphicsLoaded()
+    self.graphicsLoaded = true
 end
 
-function BaseCharacter.client_onGraphicsUnloaded( self )
-	self.graphicsLoaded = false
-
-
-
-
-
-
-
-
-
-
-
+function BaseCharacter:client_onGraphicsUnloaded()
+    self.graphicsLoaded = false
 end
 
-function BaseCharacter.client_onFixedUpdate( self, deltaTime )
-	if not sm.isHost then
-		self.cl.waterMovementSpeedFraction = 1.0
-		if self.character.clientPublicData and self.character.clientPublicData.waterMovementSpeedFraction then
-			self.cl.waterMovementSpeedFraction = self.character.clientPublicData.waterMovementSpeedFraction
-		end
-	end
+---Updates self.cl.waterMovementSpeedFraction with clientPublicData
+function BaseCharacter:client_onFixedUpdate()
+    if not sm.isHost then
+        self.cl.waterMovementSpeedFraction = 1.0
+        if self.character.clientPublicData and self.character.clientPublicData.waterMovementSpeedFraction then
+            self.cl.waterMovementSpeedFraction = self.character.clientPublicData.waterMovementSpeedFraction
+        end
+    end
 end
 
-function BaseCharacter.client_onUpdate( self, deltaTime )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	local totalMovementSpeedFraction = self.cl.waterMovementSpeedFraction
-	self.character.movementSpeedFraction = totalMovementSpeedFraction
-
-	if not self.graphicsLoaded then
-		return
-	end
-
+function BaseCharacter:client_onUpdate()
+    local totalMovementSpeedFraction = self.cl.waterMovementSpeedFraction
+    self.character.movementSpeedFraction = totalMovementSpeedFraction
+
+    if not self.graphicsLoaded then
+        return
+    end
 end
